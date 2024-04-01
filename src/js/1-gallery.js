3a27ebd0ca1cc8a75c2d3cca849f05eaa2c1d6ae
Valentyn-Melnyk <v.iv.melnyk@gmail.com>
2024-04-01 23:10:08 +0300

    fix-setting-2

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,3 +1,13 @@
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  /* options */
+});
+console.log(SimpleLightbox);
+
 const images = [
   {
     preview:
@@ -79,7 +89,7 @@ function createGallery(images) {
       alt="${description}"
     />
   </a>
-</li>`,
+</li>`
     )
     .join('');
 }
